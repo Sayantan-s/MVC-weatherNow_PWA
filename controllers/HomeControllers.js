64e87e63a270911dfae647240bd455f2cacb8def
Sayantan Samanta <sssamanta789@gmail.com>
2021-04-06 22:52:45 +0530

    Added typescript config

--- controllers/HomeControllers.js
+++ controllers/HomeControllers.js
@@ -6,7 +6,10 @@ const date = new Date();
 const today = date.toDateString().split(' ');
 today.pop();
 
-const time = date.toTimeString().split(' ')[0].split(':')[0];
+const time = date
+.toTimeString()
+.split(' ')[0]
+.split(':')[0];
 
 const checkTime = (time > 5 && time < 17) ? 'day' : 'night'
 
